07c9c629d7705b88bbe8e44f6700125ae4ffb44b
nazonohito51 <nazonohito51@gmail.com>
2019-11-27 09:39:09 +0900

    fix PayClassifications

--- src/PayrollSystem/Domain/ValueObjects/PayClassification/PayClassification.php
+++ src/PayrollSystem/Domain/ValueObjects/PayClassification/PayClassification.php
@@ -3,7 +3,9 @@ declare(strict_types=1);
 
 namespace PayrollSystem\Domain\ValueObjects\PayClassification;
 
+use PayrollSystem\Domain\ValueObjects\PayClassification\PayDaySpecification\PayDaySpecificationInterface;
+
 interface PayClassification
 {
-
+    public function getPayDaySpecification(): PayDaySpecificationInterface;
 }
--- src/PayrollSystem/Domain/ValueObjects/PayClassification/SalariedClassification.php
+++ src/PayrollSystem/Domain/ValueObjects/PayClassification/SalariedClassification.php
@@ -5,6 +5,7 @@ namespace PayrollSystem\Domain\ValueObjects\PayClassification;
 
 use PayrollSystem\Domain\Exceptions\InvalidArgumentException;
 use PayrollSystem\Domain\ValueObjects\PayClassification\PayClassification;
+use PayrollSystem\Domain\ValueObjects\PayClassification\PayDaySpecification\PayDaySpecificationInterface;
 
 class SalariedClassification implements PayClassification
 {
@@ -16,5 +17,10 @@ class SalariedClassification implements PayClassification
             throw new InvalidArgumentException();
         }
         $this->salaryRate = $salaryRate;
+    }
+
+    public function getPayDaySpecification(): PayDaySpecificationInterface
+    {
+        // TODO: Implement getPayDaySpecification() method.
     }
 }

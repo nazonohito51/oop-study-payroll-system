3a3e17b45d6c5f98a0e8b452967e8f5dc553030c
SatoshiKawashima <nazonohito51@gmail.com>
2019-11-27 10:15:31 +0900

    add factory

---
+++ src/PayrollSystem/Domain/Factories/EmployeeFactory.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+namespace PayrollSystem\Domain\Factories;
+
+use PayrollSystem\Domain\Entities\Employee;
+use PayrollSystem\Domain\ValueObjects\Address;
+use PayrollSystem\Domain\ValueObjects\Identifier\EmployeeId;
+use PayrollSystem\Domain\ValueObjects\Name;
+use PayrollSystem\Domain\ValueObjects\PayClassification\CommissionedClassification;
+use PayrollSystem\Domain\ValueObjects\PayClassification\HourlyClassification;
+use PayrollSystem\Domain\ValueObjects\PayClassification\SalariedClassification;
+
+class EmployeeFactory
+{
+    public function createHourlyEmployee(string $employeeId, string $name, string $address, int $hourlyRate): Employee
+    {
+        $employeeId = new EmployeeId($employeeId);
+        $employeeName = new Name($name);
+        $employeeAddress = new Address($address);
+
+        return new Employee($employeeId, $employeeName, $employeeAddress, new HourlyClassification($hourlyRate));
+    }
+
+    public function createSalaryEmployee(string $employeeId, string $name, string $address, int $monthlySalary): Employee
+    {
+        $employeeId = new EmployeeId($employeeId);
+        $employeeName = new Name($name);
+        $employeeAddress = new Address($address);
+
+        return new Employee($employeeId, $employeeName, $employeeAddress, new SalariedClassification($monthlySalary));
+    }
+
+    public function createCommissionedEmployee(string $employeeId, string $name, string $address, int $salaryRate, int $commissionedRate)
+    {
+        $employeeId = new EmployeeId($employeeId);
+        $employeeName = new Name($name);
+        $employeeAddress = new Address($address);
+
+        return new Employee($employeeId, $employeeName, $employeeAddress, new CommissionedClassification($salaryRate, $commissionedRate));
+    }
+}
